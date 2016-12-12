4840e755c31f8a025254f5a432c4c11c5bdf1409
ravindra kumar <ravindracs1986@gmail.com>
2016-12-12 21:23:44 +0530

    added model class

---
+++ smsaware/src/main/java/com/smsaware/model/Registration.java
@@ -0,0 +1,52 @@
+package com.smsaware.model;
+
+public class Registration extends Login{
+
+	private Integer mobileNumber;
+	private String fname;
+	private String lname;
+	private Address address;
+	private String email;
+	private String gender;
+	public Integer getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(Integer mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+	
+	
+	
+}
