0472d17d2bffa4c4792eea070b60d19c103584fc
ravindra kumar <ravindracs1986@gmail.com>
2017-01-12 19:23:37 +0530

    changed address table name

--- smsaware/src/main/java/com/smsaware/utils/DataBaseQuerys.java
+++ smsaware/src/main/java/com/smsaware/utils/DataBaseQuerys.java
@@ -5,7 +5,7 @@ public class DataBaseQuerys {
 	public final static String getUserByEmail="select * from user_registration where email =? and password=?";
 	public final static String getUserByPhone="select * from user_registration where phone =? and password=?";
 	public final static String registerUser="insert into user_registration(NAME,BIRTH_DATE,GENDER,NATIONALITY,WEBSITE,NO_OF_SMS,EMAIL,PASSWORD,PHONE) values(?,?,?,?,?,?,?,?,?)";
-	public final static String registerAddress="insert into Address(USER_ADDRESS,STREET,CITY,STATE,ZIPCODE,userId) values(?,?,?,?,?,?)";
+	public final static String registerAddress="insert into address(USER_ADDRESS,STREET,CITY,STATE,ZIPCODE,userId) values(?,?,?,?,?,?)";
 	public final static String checkUser="select * from user_registration where email =? or phone=?";
 	public final static String otpQuery=" insert into user_otp(userId, OTP, otpTime, isAuthenticated) values (?, ?, ?, ?)";
 	public final static String userOTPCheck="select * from user_otp where OTP=?";
